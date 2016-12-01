3716e562e19a3a0f1f354fbbca1018652eabdc98
joelcowie <joel.a.cowie@gmail.com>
2016-12-01 13:48:42 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(new_words)
+    matching_words = []
+    new_words.each do |word|
+      if word.split("").sort == @word.split("").sort
+        matching_words << word.split(" ")
+      end
+    end
+    return matching_words.flatten
+  end
+end
